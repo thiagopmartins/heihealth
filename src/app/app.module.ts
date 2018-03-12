6b2c779fa678090539a89bace66cc0b889f602e2
Thiago Martins <thiagopmartins@outlook.com>
2018-03-11 21:24:56 -0300

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { ClarityModule } from "@clr/angular";
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    ClarityModule,
     FormsModule,
     HttpModule
   ],
